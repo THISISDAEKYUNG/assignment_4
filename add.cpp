e2bfce8969f5625ea2140a827aaf118c34b18ecc
THISISDAEKYUNG <sinseolove@gmail.com>
2025-03-11 15:50:31 +0900

    control flow initialize

---
+++ add.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int c = 0;
+int d = 100;
+
+int main()
+
+{
+    cout << "Hello, world!" << endl;
+
+    int a = 4;
+    int b = 2;
+    int c = a * b;
+
+    cout << "1/2 * x = 4, x = ?" << endl;
+    cout << " x = " << c << endl;
+
+    return 0;
+}
